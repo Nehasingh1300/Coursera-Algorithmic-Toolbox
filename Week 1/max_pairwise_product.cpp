232f1eab8e651d38fd4a5763ed8e07fc15b13672
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-28 11:44:17 +0530

    With some stress testing to check the code with test cases

---
+++ Week 1/max_pairwise_product.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long MaxPairwiseProduct(const vector<int>& numbers) {
+    long long max_product = 0;
+    int n = numbers.size();
+
+    for (int first = 0; first < n; ++first) {
+        for (int second = first + 1; second < n; ++second) {
+            if(((long long)numbers[i]) > max_product) {
+		max_product = ((long long)(numbers[first])) * numbers[second];
+             }
+	}
+    }
+    return max_product;
+}
+
+long long MaxPairwiseProductFast(const vector<int>& numbers) {
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for( int i=0; i<n; ++i)
+	if((max_index1 == -1 ) || (numbers[i] > numbers[max_index1]))
+		max_index1=i;    
+
+     int max_index2 = -1;
+     for( int j=0; j<n; ++j)
+	if((j != max_index1 ) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+		max_index2=j;
+
+      return ((long long) (numbers[max_index1]))*numbers[max_index2];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+
+    long long result =  MaxPairwiseProductFast(numbers);
+    cout << result << "\n";
+    return 0;
+}
